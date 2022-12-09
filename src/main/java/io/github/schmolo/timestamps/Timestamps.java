916690c320509bb09de264840341f6c8ea9e3e63
PolyLogic64 <50244580+PolyLogic64@users.noreply.github.com>
2022-12-09 14:34:54 +0100

    pushing p 3

--- src/main/java/io/github/schmolo/timestamps/Timestamps.java
+++ src/main/java/io/github/schmolo/timestamps/Timestamps.java
@@ -1,13 +1,14 @@
 package io.github.schmolo.timestamps;
 
 import io.github.schmolo.timestamps.events.betterwhisper;
-import io.github.schmolo.timestamps.events.chatcolor;
 import io.github.schmolo.timestamps.events.playerchatevent;
 import io.github.schmolo.timestamps.events.serversayevent;
 import net.md_5.bungee.api.ChatColor;
-
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 
 public final class Timestamps extends JavaPlugin {
 
@@ -15,15 +16,31 @@ public final class Timestamps extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+
+
+        File file = new File(Bukkit.getServer().getPluginManager().getPlugin("Timestamps").getDataFolder() + "/playernames.yml");
+
+        if(!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) { }
+        }
+
+        TimsLib.setConfig(getConfig().getBoolean("debug_mode"));
+
+
         getServer().getPluginManager().registerEvents(new playerchatevent(), this);
 
         getCommand("say").setExecutor(new serversayevent());
 
         getCommand("msg").setExecutor(new betterwhisper());
 
-        this.getCommand("chatcolor").setExecutor(new chatcolor());
-
-        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[TimeStamps v0.0.69]: enabled");
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[TimeStamps]: enabled");
+        if(getConfig().getBoolean("debug_mode")) {
+            getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "[TimeStamps] DEBUG MODE: enabled");
+        }
     }
 
     @Override
@@ -32,9 +49,10 @@ public final class Timestamps extends JavaPlugin {
 
         getServer().getConsoleSender().sendMessage(ChatColor.RED + "[TimeStamps]: disabled");
     }
+
+
 }
 
 /*
-    TODO: Implement Config file system
     TODO: Add way to edit colors for Player names ( not only chat )
 */
